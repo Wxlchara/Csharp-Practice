e2bc3437bb04b2e81b2cc11f61882f5e2294bfef
Wxlchara <anton.iyj.anton@gmail.com>
2026-05-21 18:06:57 +0300

    Add files via upload

---
+++ Game.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21._05._2026.Yadro
+{
+    public class Game
+    {
+        public int Rows { get; }
+        public int Cols { get; }
+
+        public int Generation { get; }
+
+        private bool[,] _grid;
+        public bool[,] Grid => (bool[,])_grid.Clone();
+
+        private bool[,] _nextGrid;
+
+        public Game(int rows = 20, int cols = 30)
+        {
+            Rows = rows;
+            Cols = cols;
+            Generation = 1;
+            _grid = new bool[Rows, Cols];
+            _nextGrid = new bool[Rows, Cols];
+        }
+
+        public void ToggleCell(int row, int col)
+        {
+
+        }
+
+        private bool IsValidCell(int row, int col)
+        {
+            if (row < 0 || row >= Rows || col < 0 || col >= Cols)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void NextGeneration()
+        {
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Cols; j++)
+                {
+                    int n = CountNeighbors(i, j);
+
+                    if (_grid[i, j])
+                    {
+                        if (n == 2 || n == 3) _nextGrid[i, j] = true;
+                        else _nextGrid[i, j] = false;
+                    }
+                    else
+                    {
+                        if (n == 3) _nextGrid[i, j] = true;
+                        else _nextGrid[i, j] = false;
+                    }
+                }
+            }
+        }
+
+        private int CountNeighbors(int row,int col)
+        {
+            int count = 0;
+            for (int i = -1; i <= 1; i++)
+            {
+                for (int j = -1; j <= 1; j++)
+                {
+                    if (i == 0 || j == 0) continue;
+
+                    int trgRow = row + i;
+                    int trgCol = col + j;
+                    if (!IsValidCell(trgRow, trgCol)) continue;
+
+                    if (_grid[trgRow, trgCol])
+                        count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using _21._05._2026.Yadro;
+
+namespace _21._05._2026
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Game _game;
+        private Border[,] _cells;
+        private Border[,] _border;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _game = new Game();
+
+            _cells = new Border[_game.Rows, _game.Cols];
+            CreateGameGrid();
+        }
+
+        private void CreateGameGrid()
+        {
+            gameGrid.Children.Clear();
+            gameGrid.Rows = _game.Rows;
+            gameGrid.Columns = _game.Cols;
+            for (int i = 0; i < _game.Rows; i++)
+            {
+                for (int j = 0; j < _game.Cols; j++)
+                {
+                    Border border = new Border()
+                    {
+                        Background = Brushes.Black,
+                        BorderBrush = Brushes.DimGray,
+                        BorderThickness = new Thickness(0.3)
+                    };
+
+                    int iCaptured = i, jCaptured = j;
+                    border.MouseLeftButtonDown += (sender, e) => CellClick(iCaptured, jCaptured);
+                }
+            }
+        }
+
+        private void StartClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("start clicked");
+        }
+        private void StopClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("stop clicked");
+        }
+    }
+}
